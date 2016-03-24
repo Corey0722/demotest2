00e7b7824c57eb7247effcefe561c5d73e961f36
Corey <corey@private.ip.shu.edu.tw>
2016-03-24 12:21:27 +0800

    Initial Commit

---
+++ DemoTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoTest2
+//
+//  Created by Corey on 2016/3/24.
+//  Copyright © 2016年 Corey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
